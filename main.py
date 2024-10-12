8e63f143855c4e28a2d1d98a6b55fcd25800f39b
AryanA3 <aryanagarkar09@gmail.com>
2024-10-12 14:57:05 +0530

    player drawn and movement controls added

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from constants import *
 import pygame
+from player import Player
 
 def main():
+
     pygame.init()
-    print('Starting asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH/2, y= SCREEN_HEIGHT/2) 
+
     while True  :
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill(color = (0,0,0))
+
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
